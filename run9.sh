1e5ac1e9e50a703b637381718e00657c1e4552da
Mark Moorcroft <mntbighker@gmail.com>
2024-05-29 20:01:26 -0700

    Update run9.sh

--- run9.sh
+++ run9.sh
@@ -25,6 +25,7 @@ rm -rf .config
 mv Linux_env/.config .
 mv Linux_env/.tmux.conf .
 echo -e "### Type exit after the oh-my-zsh install script finishes, to complete setup ###\n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
